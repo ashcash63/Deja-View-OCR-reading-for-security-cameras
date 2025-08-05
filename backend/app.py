5c506f9c0abfb0b08aba516bc12dd0c84cb117e8
aashi <97696617+ashcash63@users.noreply.github.com>
2025-08-05 01:29:43 +0000

    added flask api routes

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 #install dependicies
-from flask import Flash, jsonify, request, send_file, after_this_request
+from flask import Flask, jsonify, request, send_file, after_this_request
 from flask_cors import CORS
 import os
 import threading
@@ -15,10 +15,10 @@ app = Flask(__name__)
 CORS = (app)
 
 #directory vars
-#video_dir = "videos"
-#screenshot_dir = "screenshots"
-#db_path = "db_data"
-#confidence_threshold = 0.8 #only save detections with > 80% confidence
+video_dir = "videos"
+screenshot_dir = "screenshots"
+db_path = "db_data"
+confidence_threshold = 0.8 #only save detections with > 80% confidence
 
 #OCR and database setup
 print("intializing main ocr reader...")
@@ -60,7 +60,7 @@ def save_text_data(text, metadata):
     
 #search queries in chromadb
 def search_text_data(query):
-    #search fro text using chromdb with a filter
+    #search for text using chromdb with a filter
     try:
         collection = client.get_collection(name = "video_text_search")
         #use where_document = {"$contains": query} for substring search
@@ -84,8 +84,8 @@ def search_text_data(query):
             formatted_result = {
                 'text': info,
                 'timestamp': meta.get('timestamp'),
-                'camera_id' = meta.get('camera_id'),
-                'source_video' = meta.get('source_video'),
+                'camera_id':meta.get('camera_id'),
+                'source_video' : meta.get('source_video'),
                 'screenshot_filename': os.path.basename(meta.get('screenshot_path', ''))
             }
             formatted_results.append(formatted_result)
@@ -95,8 +95,9 @@ def search_text_data(query):
         print(f"Error querying ChromaDB: {e}")
         return []
 
-    def capture_screenshot_with_highlight(frame,bbox,text,timestamp, camera_id):
-        #don't want to draw on original
+def capture_screenshot_with_highlight(frame,bbox,text,timestamp, camera_id):
+    #don't want to draw on original
+    try:
         highlighted_frame = frame.copy()
         #bounding box top left and bottom right corners
         (tl, tr, br, bl) = bbox;
@@ -119,7 +120,7 @@ def search_text_data(query):
         print(f"Error saving highlighted screenshot: {e}")
         return None
 
-#Multiprocessing functions
+#---Multiprocessing functions---
 
 #Global variable for each worker 
 #prep each parallel worker process for OCR tasks during multiprocessing
@@ -129,7 +130,10 @@ worker_reader = None #hold an EasyOCR.Reader
 def init_worker():
     """Intializer for each worker process in the pool"""
     global worker_reader
+    print(f"Initializing EasyOCR reader in worker process {os.getpid()}...")
+    # Each process gets its own reader instance to avoid conflicts
     worker_reader = easyocr.Reader(['en'], gpu=False)
+    print(f"Worker {os.getpid()} initialized.")
 
 
 #most important function 
@@ -144,34 +148,170 @@ def process_video_task(video_path):
     camera_id = os.path.splitext(video_file)[0] #use filename as camera_id (remove .mp4)
     interval_seconds = 1 #process 1 frame/sec, only run OCR every 1 second of video time
 
-    #open video
-    cap = cv2.VideoCapture(video_path) #open video using OpenCV
-    if not cap.isOpened():
-        print(f"Error: Could not open video file {video_path}")
-        return
+    try:
+        #open video
+        cap = cv2.VideoCapture(video_path) #open video using OpenCV
+        if not cap.isOpened():
+            print(f"Error: Could not open video file {video_path}")
+            return
 
-    #cap = capture refere to an instance of OpenCV's VideoCapture object
-    #calculate frame interval in 30 FPS video (default)
-    #only analyze 1 frame so skip 29 frames (to save compute)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    if fps == 0: fps = 30
-    frames_to_skip = int(fps * interval_seconds)
+        #cap = capture refere to an instance of OpenCV's VideoCapture object
+        #calculate frame interval in 30 FPS video (default)
+        #only analyze 1 frame so skip 29 frames (to save compute)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        if fps == 0: fps = 30
+        frames_to_skip = int(fps * interval_seconds)
     
-    #loop thru video frames
-    frame_count = 0
-    while True:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_count)
-        success, frame = cap.read()
-        if not success:
-            break
-    #run ocr on frame
-        ocr_results = worker_reader.readtext(frame)
-        curr_timestamp = frame_count / fps
+        #loop thru video frames
+        frame_count = 0
+        while True:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_count)
+            success, frame = cap.read()
+            if not success:
+                break
+            #run ocr on frame
+            ocr_results = worker_reader.readtext(frame)
+            curr_timestamp = frame_count / fps #what time it showed up
 
-        #filter and save OCR results
+            for (bbox, text, confidence) in ocr_results:
+                if confidence >= confidence_threshold:
+                    #save highlighted screenshot + metadata
+                    screenshot_path = os.path.basename(screenshot_dir)
+                    metadata = {'timestamp': curr_timestamp,
+                                'camera_id': camera_id,
+                                'source_video' : video_file,
+                                'confidence' : confidence,
+                                'screenshot_path' : screenshot_path or ''}
+                    save_text_data(text, metadata)
+            #move to next frame
+            frame_count += frames_to_skip
+        print(f"Completed proceswsing {video_file}")
+    except Exception as e:
+        print(f"Error in processing video for {video_file}: {e}")
+    #cleanup
+    finally:
+        if 'cap' in locals() and cap.isOpened():
+            cap.release()
+
+def process_all_video_multiprocess():
+    """Finds all videos and processes them using a multiprocessing pool."""
+    print("\n--- Starting Automatic Video Processing using Multiprocessing ---")
+
+    try:
+        #cleanup old ocr results from chromaDB
+        client.delete_collection(name = "video_text_search")
+        client.get_or_create_collection(name="video_text_search")
+        print("Collection cleared and recreated.")
+    except Exception as e:
+        print(f"Could not clear collection (it may not have existed): {e}")
+        # Ensure the collection exists for the workers
+        client.get_or_create_collection(name="video_text_search")
+    
+    video_files = [os.path.join(video_dir, f) for f in os.listdir(video_dir) if f.endswith(('.mp4', '.mov', '.avi'))]
+    if not video_files:
+        print("No video files found in the 'videos' directory.")
+        return
+    
+    print(f"Found {len(video_files)} videos to process: {[os.path.basename(f) for f in video_files]}")
+    #use multiprocessing pool to process videos in parallel, limit to 4 videos at a time
+    num_processes = min(multiprocessing.cpu_count(), 4)
+    with multiprocessing.Pool(processes = num_processes, intializer=init_worker) as pool:
+        #create a pool of worker process each one runs init worker settingf up easy ocr instance
+        pool.map(process_video_task, video_files)
+    print("all video processing taasks dispatched")
+
+
+    #--Flask API routes-- (post to send and get to fetch)
+    @app.route('/api/process', methods = ['POST'])
+    def process_video():
+        #enpoint to processing for single video
+        data = request.json
+        video_file = data.get('video_file')
+        if not video_file:
+            return jsonify({'error': 'No video_file provided'}), 400
+        video_path = os.path.join(video_dir, video_file)
+        if not os.path.exists(video_path):
+            return jsonify({"error": f"Video file not found: {video_path}"}), 404
+        try:
+            #cannot call process_video_task(video_path) directly, API would freeze until finished
+        #create bg thread that will run the process_video_task in background without blocking API resposne
+            thread = threading.Thread(target=process_video_task, args= (video_path,))
+            thread.daemon = True #(thread auto shut down when main program exits)
+            thread.start()
+
+            return jsonify({
+                "status": "processing", 
+                "video": video_file,
+                "message": "video processing started in background"
+            })
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+    
+
+    @app.route('/api/search', method=['GET'])
+    # Common status codes: 400 = bad request, 500 = Internal server error, 404 = Not Found
+    def search_text():
+        #endpoint for searching text in videos
+        query = request.args.get('query', '')
+        if not query:
+            return jsonify({'error': 'No query provided'}), 400
+        
+        try: 
+            results = search_text_data(query)
+            return jsonify({"results": results})
+        
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+        
+    @app.route('/api/screenshot/<filename>', methods=['GET'])
+    def get_screenshot(filename):
+        #endpoint for screenshot images
+
+        screenshot_dir = "screenshots"
+
+        if '..' in filename or '/' in filename:
+            return jsonify({"error": "Invalid filename"}), 400
+        
+        file_path = os.path.join(screenshot_dir, filename)
+
+        if os.path.exists(file_path):
+            return send_file(file_path, mimetype='image/png')
+        else:
+            #return 404, for demo find other filename
+            for file in os.listdir(screenshot_dir) if os.path.exits(screenshot_dir)
+
+    @app.route('/api/list_videos/<filename>', methods=['GET'])
+    def list_videos():
+
+    @app.route('/api/video_segment/<filename>', methods=['GET'])
+    def get_video_segment():
+
+    @app.route('/api/index/<filename>', methods=['GET'])
+    def index():
+
+    if __name__ == '__main__':
+        #needed on windows
+         multiprocessing.freeze_support()
+    
+        print("Starting MSI Video Tracker API server...")
+    
+        # Start video processing at startup
+        process_all_videos_multiprocess()
+    
+        # Now start the Flask app
+        app.run(debug=True, port=5000, use_reloader=False)
+
+    
+
         
 
-    cap.release()
+
+    
+
+
+
+
+    
 
 
 
